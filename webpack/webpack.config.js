92febc203d1176e7a3c7167bd70bdca86695af50
chansung park <chansung@chansungs-MacBook-Pro.local>
2022-06-01 02:41:02 +0900

    update webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
    mode: "production",
    entry: {
-      background: path.resolve(__dirname, "..", "src", "contentScript.ts"),
+      contentScript: path.resolve(__dirname, "..", "src", "contentScript.ts"),
    },
    output: {
       path: path.join(__dirname, "../dist"),
